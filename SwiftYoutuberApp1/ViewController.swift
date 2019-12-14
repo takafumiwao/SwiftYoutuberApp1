a5283683c6bf24d813108ebae88ddd5ec3b31af5
Takafumi Iwao <koshiiwao2904@gmail.com>
2019-12-14 13:33:06 +0900

    Initial Commit

---
+++ SwiftYoutuberApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftYoutuberApp1
+//
+//  Created by 岩男高史 on 2019/12/14.
+//  Copyright © 2019 岩男高史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
